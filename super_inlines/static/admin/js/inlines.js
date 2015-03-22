06820b25c0cc90becb77071f3202c6bc1dd2a9a7
Bertrand Bordage <bordage.bertrand@gmail.com>
2015-03-22 19:36:21 +0100

    Rewrites inlines.js to be less Voodoo and more DRY.

--- super_inlines/static/admin/js/inlines.js
+++ super_inlines/static/admin/js/inlines.js
@@ -15,80 +15,78 @@
  * See: http://www.opensource.org/licenses/bsd-license.php
  */
 (function($) {
+
+  function fillAttrPlaceholders($elements, prefix, index) {
+    var id_regex = new RegExp("(" + prefix + "-(\\d+|__prefix__))");
+    var replacement = prefix + "-" + index;
+    $.each(['for', 'id', 'name'], function(i, attrName) {
+      $elements.each(function() {
+        var $el = $(this), attr = $el.attr(attrName);
+        if (attr) {
+          $el.attr(attrName, attr.replace(id_regex, replacement));
+        }
+      });
+    });
+  }
+
   $.fn.formset = function(opts) {
     var options = $.extend({}, $.fn.formset.defaults, opts);
-    var $this = $(this);
-    var $parent = $this.parent();
-    var updateElementIndex = function(el, prefix, ndx) {
-      var id_regex = new RegExp("(" + prefix + "-(\\d+|__prefix__))");
-      var replacement = prefix + "-" + ndx;
-      if ($(el).prop("for")) {
-        $(el).prop("for", $(el).prop("for").replace(id_regex, replacement));
-      }
-      if (el.id) {
-        el.id = el.id.replace(id_regex, replacement);
-      }
-      if (el.name) {
-        el.name = el.name.replace(id_regex, replacement);
-      }
-    };
-    var totalForms = $("#id_" + options.prefix + "-TOTAL_FORMS").prop("autocomplete", "off");
-    var nextIndex = parseInt(totalForms.val(), 10);
-    var maxForms = $("#id_" + options.prefix + "-MAX_NUM_FORMS").prop("autocomplete", "off");
+    var $parent = this.parent();
+    var $totalForms = $("#id_" + options.prefix + "-TOTAL_FORMS").prop("autocomplete", "off");
+    var nextIndex = parseInt($totalForms.val(), 10);
+    var $maxForms = $("#id_" + options.prefix + "-MAX_NUM_FORMS").prop("autocomplete", "off");
     // only show the add button if we are allowed to add more items,
         // note that max_num = None translates to a blank string.
-    var showAddButton = maxForms.val() === '' || (maxForms.val()-totalForms.val()) > 0;
-    $this.each(function(i) {
+    var showAddButton = $maxForms.val() === '' || ($maxForms.val()-$totalForms.val()) > 0;
+    this.each(function() {
       $(this).not("." + options.emptyCssClass).addClass(options.formCssClass);
     });
-    if ($this.length && showAddButton) {
-      var addButton;
-      if ($this.prop("tagName") == "TR") {
+    if (this.length && showAddButton) {
+      var $addButton;
+      if (this.is('tr')) {
         // If forms are laid out as table rows, insert the
         // "add" button in a new table row:
-        var numCols = this.eq(-1).children().length;
+        var numCols = this.last().children().length;
         $parent.append('<tr class="' + options.addCssClass + '"><td colspan="' + numCols + '"><a href="javascript:void(0)">' + options.addText + "</a></tr>");
-        addButton = $parent.find("tr:last a");
+        $addButton = $parent.find("tr:last a");
       } else {
         // Otherwise, insert it immediately after the last form:
-        $this.filter(":last").after('<div class="' + options.addCssClass + '"><a href="javascript:void(0)">' + options.addText + "</a></div>");
-        addButton = $this.filter(":last").next().find("a");
+        this.last().after('<div class="' + options.addCssClass + '"><a href="javascript:void(0)">' + options.addText + "</a></div>");
+        $addButton = this.last().next().find("a");
       }
-      addButton.click(function(e) {
+      $addButton.click(function(e) {
         e.preventDefault();
-        var totalForms = $("#id_" + options.prefix + "-TOTAL_FORMS");
-        var template = $("#" + options.prefix + "-empty");
-        var row = template.clone(true);
-        row.removeClass(options.emptyCssClass)
+        var $totalForms = $("#id_" + options.prefix + "-TOTAL_FORMS");
+        var $template = $("#" + options.prefix + "-empty");
+        var $row = $template.clone(true);
+        $row.removeClass(options.emptyCssClass)
           .addClass(options.formCssClass)
           .attr("id", options.prefix + "-" + nextIndex);
-        if (row.is("tr")) {
+        if ($row.is('tr')) {
           // If the forms are laid out in table rows, insert
           // the remove button into the last table cell:
-          row.children(":last").append('<div><a class="' + options.deleteCssClass +'" href="javascript:void(0)">' + options.deleteText + "</a></div>");
-        } else if (row.is("ul") || row.is("ol")) {
+          $row.children(":last").append('<div><a class="' + options.deleteCssClass +'" href="javascript:void(0)">' + options.deleteText + "</a></div>");
+        } else if ($row.is('ul, ol')) {
           // If they're laid out as an ordered/unordered list,
           // insert an <li> after the last list item:
-          row.append('<li><a class="' + options.deleteCssClass +'" href="javascript:void(0)">' + options.deleteText + "</a></li>");
+          $row.append('<li><a class="' + options.deleteCssClass +'" href="javascript:void(0)">' + options.deleteText + "</a></li>");
         } else {
           // Otherwise, just insert the remove button as the
           // last child element of the form's container:
-          row.children(":first").append('<span><a class="' + options.deleteCssClass + '" href="javascript:void(0)">' + options.deleteText + "</a></span>");
+          $row.children(":first").append('<span><a class="' + options.deleteCssClass + '" href="javascript:void(0)">' + options.deleteText + "</a></span>");
         }
-        row.find("*").each(function() {
-          updateElementIndex(this, options.prefix, totalForms.val());
-        });
+        fillAttrPlaceholders($row.find("*"), options.prefix, $totalForms.val());
         // Insert the new form when it has been fully edited
-        row.insertBefore($(template));
+        $row.insertBefore($template);
         // Update number of total forms
-        $(totalForms).val(parseInt(totalForms.val(), 10) + 1);
+        $totalForms.val(parseInt($totalForms.val(), 10) + 1);
         nextIndex += 1;
         // Hide add button in case we've hit the max, except we want to add infinitely
-        if ((maxForms.val() !== '') && (maxForms.val()-totalForms.val()) <= 0) {
-          addButton.parent().hide();
+        if (($maxForms.val() !== '') && ($maxForms.val()-$totalForms.val()) <= 0) {
+          $addButton.parent().hide();
         }
         // The delete button of each row triggers a bunch of other things
-        row.find("a." + options.deleteCssClass).click(function(e) {
+        $row.find("a." + options.deleteCssClass).click(function(e) {
           e.preventDefault();
           // Remove the parent form containing this button:
           var row = $(this).parents("." + options.formCssClass);
@@ -99,25 +97,19 @@
             options.removed(row);
           }
           // Update the TOTAL_FORMS form count.
-          var forms = $("." + options.formCssClass);
-          $("#id_" + options.prefix + "-TOTAL_FORMS").val(forms.length);
+          var $forms = $("." + options.formCssClass);
+          $("#id_" + options.prefix + "-TOTAL_FORMS").val($forms.length);
           // Show add button again once we drop below max
-          if ((maxForms.val() === '') || (maxForms.val()-forms.length) > 0) {
-            addButton.parent().show();
+          if (($maxForms.val() === '') || ($maxForms.val()-$forms.length) > 0) {
+            $addButton.parent().show();
           }
-          // Also, update names and ids for all remaining form controls
-          // so they remain in sequence:
-          for (var i=0, formCount=forms.length; i<formCount; i++)
-          {
-            updateElementIndex($(forms).get(i), options.prefix, i);
-            $(forms.get(i)).find("*").each(function() {
-              updateElementIndex(this, options.prefix, i);
-            });
-          }
+          // Also, fill placeholders in attributes
+          // for all remaining form controls so they remain in sequence:
+          fillAttrPlaceholders($forms.find('*').addBack(), options.prefix, i);
         });
         // If a post-add callback was supplied, call it with the added form:
         if (options.added) {
-          options.added(row);
+          options.added($row);
         }
       });
     }
@@ -137,47 +129,46 @@
     removed: null          // Function called each time a form is deleted
   };
 
+  function reinitDateTimeShortCuts() {
+    // Reinitialize the calendar and clock widgets by force
+    if (typeof DateTimeShortcuts != 'undefined') {
+      $('.datetimeshortcuts').remove();
+      DateTimeShortcuts.init();
+    }
+  }
+
+  function updateSelectFilter() {
+    // If any SelectFilter widgets are a part of the new form,
+    // instantiate a new SelectFilter instance for it.
+    if (typeof SelectFilter != 'undefined'){
+      $('.selectfilter').each(function(){
+        var namearr = this.name.split('-');
+        SelectFilter.init(this.id, namearr[namearr.length-1], false);
+      });
+      $('.selectfilterstacked').each(function(){
+        var namearr = this.name.split('-');
+        SelectFilter.init(this.id, namearr[namearr.length-1], true);
+      });
+    }
+  }
 
   // Tabular inlines ---------------------------------------------------------
   $.fn.tabularFormset = function(options) {
-    var $rows = $(this);
-    var alternatingRows = function(row) {
-      $($rows.selector).not(".add-row").removeClass("row1 row2")
+    var rowsSelector = this.selector;
+    var alternatingRows = function() {
+      $(rowsSelector).not(".add-row").removeClass("row1 row2")
         .filter(":even").addClass("row1").end()
         .filter(":odd").addClass("row2");
     };
 
-    var reinitDateTimeShortCuts = function() {
-      // Reinitialize the calendar and clock widgets by force
-      if (typeof DateTimeShortcuts != "undefined") {
-        $(".datetimeshortcuts").remove();
-        DateTimeShortcuts.init();
-      }
-    };
-
-    var updateSelectFilter = function() {
-      // If any SelectFilter widgets are a part of the new form,
-      // instantiate a new SelectFilter instance for it.
-      if (typeof SelectFilter != 'undefined'){
-        $('.selectfilter').each(function(index, value){
-          var namearr = value.name.split('-');
-          SelectFilter.init(value.id, namearr[namearr.length-1], false);
-        });
-        $('.selectfilterstacked').each(function(index, value){
-          var namearr = value.name.split('-');
-          SelectFilter.init(value.id, namearr[namearr.length-1], true);
-        });
-      }
-    };
-
-    var initPrepopulatedFields = function(row) {
-      row.find('.prepopulated_field').each(function() {
+    var initPrepopulatedFields = function($row) {
+      $row.find('.prepopulated_field').each(function() {
         var field = $(this),
             input = field.find('input, select, textarea'),
             dependency_list = input.data('dependency_list') || [],
             dependencies = [];
         $.each(dependency_list, function(i, field_name) {
-          dependencies.push('#' + row.find('.field-' + field_name).find('input, select, textarea').attr('id'));
+          dependencies.push('#' + $row.find('.field-' + field_name).find('input, select, textarea').attr('id'));
         });
         if (dependencies.length) {
           input.prepopulate(dependencies, input.attr('maxlength'));
@@ -185,7 +176,7 @@
       });
     };
 
-    $rows.formset({
+    this.formset({
       prefix: options.prefix,
       addText: options.addText,
       formCssClass: "dynamic-" + options.prefix,
@@ -193,57 +184,34 @@
       deleteText: options.deleteText,
       emptyCssClass: "empty-form",
       removed: alternatingRows,
-      added: function(row) {
-        initPrepopulatedFields(row);
+      added: function($row) {
+        initPrepopulatedFields($row);
         reinitDateTimeShortCuts();
         updateSelectFilter();
-        alternatingRows(row);
+        alternatingRows();
       }
     });
 
-    return $rows;
+    return this;
   };
 
   // Stacked inlines ---------------------------------------------------------
   $.fn.stackedFormset = function(options) {
-    var $rows = $(this);
-    var updateInlineLabel = function(row) {
-      $($rows.selector).find(".inline_label").each(function(i) {
-        var count = i + 1;
-        $(this).html($(this).html().replace(/(#\d+)/g, "#" + count));
+    var rowsSelector = this.selector;
+    var updateInlineLabel = function() {
+      $(rowsSelector).find(".inline_label").each(function(i) {
+        $(this).html($(this).html().replace(/(#\d+)/g, "#" + (i + 1)));
       });
     };
 
-    var reinitDateTimeShortCuts = function() {
-      // Reinitialize the calendar and clock widgets by force, yuck.
-      if (typeof DateTimeShortcuts != "undefined") {
-        $(".datetimeshortcuts").remove();
-        DateTimeShortcuts.init();
-      }
-    };
-
-    var updateSelectFilter = function() {
-      // If any SelectFilter widgets were added, instantiate a new instance.
-      if (typeof SelectFilter != "undefined"){
-        $(".selectfilter").each(function(index, value){
-          var namearr = value.name.split('-');
-          SelectFilter.init(value.id, namearr[namearr.length-1], false);
-        });
-        $(".selectfilterstacked").each(function(index, value){
-          var namearr = value.name.split('-');
-          SelectFilter.init(value.id, namearr[namearr.length-1], true);
-        });
-      }
-    };
-
-    var initPrepopulatedFields = function(row) {
-      row.find('.prepopulated_field').each(function() {
+    var initPrepopulatedFields = function($row) {
+      $row.find('.prepopulated_field').each(function() {
         var field = $(this),
             input = field.find('input, select, textarea'),
             dependency_list = input.data('dependency_list') || [],
             dependencies = [];
         $.each(dependency_list, function(i, field_name) {
-          dependencies.push('#' + row.find('.form-row .field-' + field_name).find('input, select, textarea').attr('id'));
+          dependencies.push('#' + $row.find('.form-row .field-' + field_name).find('input, select, textarea').attr('id'));
         });
         if (dependencies.length) {
           input.prepopulate(dependencies, input.attr('maxlength'));
@@ -251,7 +219,7 @@
       });
     };
 
-    $rows.formset({
+    this.formset({
       prefix: options.prefix,
       addText: options.addText,
       formCssClass: "dynamic-" + options.prefix,
@@ -259,14 +227,14 @@
       deleteText: options.deleteText,
       emptyCssClass: "empty-form",
       removed: updateInlineLabel,
-      added: (function(row) {
-        initPrepopulatedFields(row);
+      added: (function($row) {
+        initPrepopulatedFields($row);
         reinitDateTimeShortCuts();
         updateSelectFilter();
-        updateInlineLabel(row);
+        updateInlineLabel();
       })
     });
 
-    return $rows;
+    return this;
   };
 })(django.jQuery);
