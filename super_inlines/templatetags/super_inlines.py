f1dcd88bf5d8057e2d5eda7bf26037ba7377f623
Bertrand Bordage <bordage.bertrand@gmail.com>
2015-04-10 18:01:02 +0200

    Avoids failing if an inline is not a super inline.

--- super_inlines/templatetags/super_inlines.py
+++ super_inlines/templatetags/super_inlines.py
@@ -3,12 +3,16 @@
 from __future__ import unicode_literals
 from django.template import Library
 
+from ..admin import SuperInlineModelAdmin
+
 
 register = Library()
 
 
 @register.assignment_tag(takes_context=True)
 def get_sub_inline_formsets(context, inline):
+    if not isinstance(inline, SuperInlineModelAdmin):
+        return ()
     request = context['request']
     formsets, inline_instances = inline._create_formsets(request, None, False)
     return inline.get_inline_formsets(request, formsets, inline_instances,
